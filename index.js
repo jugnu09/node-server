4c5b771faab4f1f8c1ad7a17ff6ff8f4dce3ddf1
Ritesh <Bisen@192.168.1.19>
2023-07-14 13:09:51 +0530

    handling cors issue

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const io = require('socket.io')((http,  { cors: { origin: '*' } }));
+// const io = require('socket.io')(8000);
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const cors = require('cors');
+
+app.use(cors());
 
 const users = {};
 io.on('connection', socket => {
@@ -18,5 +25,9 @@ io.on('connection', socket => {
     socket.on('disconnect', message=> {
         socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id]
-    } )
+    })
+
+    server.listen(8000, () => {
+        console.log('Server running on port 8000');
+    });
 })
